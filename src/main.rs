d82a4c592bd32d1340bfd497b8b63a3e04f2eeb7
Franklin Webber <franklin.webber@gmail.com>
2018-04-26 17:24:34 -0500

    Completes more of the CRUD
    * Fixes the initial launch to check for existing records before
      trying to shove things into it.
    * The create and the update action needed to return a Json<Value>
    * Adds to the Scan Struct some methods for create and delete
    * Adds the ChangeSet macro def to the Scan
    * Re-order the modules, crates, and usings at the top
    
    Signed-off-by: Franklin Webber <franklin@chef.io>

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,23 @@
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
 
-#[macro_use] extern crate diesel;
 extern crate dotenv;
-
-extern crate rocket;
-#[macro_use] extern crate rocket_contrib;
-#[macro_use] extern crate serde_derive;
-
-use rocket_contrib::{Json, Value};
-
-use diesel::prelude::*;
 use dotenv::dotenv;
 use std::env;
 
+extern crate rocket;
+use rocket_contrib::{Json, Value};
+
+#[macro_use] extern crate diesel;
+use diesel::prelude::*;
+
+#[macro_use] extern crate rocket_contrib;
+#[macro_use] extern crate serde_derive;
+
 mod schema;
+use schema::scans;
 mod scan;
 use scan::{Scan};
-use schema::scans;
 
 pub fn establish_connection() -> SqliteConnection {
     dotenv().ok();
@@ -27,53 +27,122 @@ pub fn establish_connection() -> SqliteConnection {
         .expect(&format!("Error connecting to {}", database_url))
 }
 
-#[post("/", data="<scan>")]
-fn create(scan: Json<Scan>) -> Json<Scan> {
-    scan
-}
-
 #[get("/")]
-fn read() -> Json<Value> {
+fn index() -> Json<Value> {
     let ordered_scans = scans::table.order(scans::id.asc())
         .load::<Scan>(&establish_connection())
         .unwrap();
-          
+  
     Json(json!(ordered_scans))
 }
 
+#[get("/<id>")]
+fn read(id: i32) -> Json<Value> {
+    let existing_scans = scans::table.filter(scans::id.eq(id)).load::<Scan>(&establish_connection());
+    
+    match existing_scans {
+        Ok(found_scans) => {
+            if found_scans.len() == 1 {
+                Json(json!(found_scans[0]))    
+            } else {
+                Json(json!({ "status" : "failed", "message" : format!("Could not find scan with index {}",id) }))
+            }
+            
+        },
+        Err(message) => {
+            Json(json!({ "status" : "failed", "message" : format!("{}",message) }))
+        }
+    }
+}
+
+#[post("/", data="<scan>")]
+fn create(scan: Json<Scan>) -> Json<Value> {
+    let insert = Scan { id: None, ..scan.into_inner() };
+    let created_scan = Scan::create(insert, &establish_connection());
+    Json(json!(created_scan))
+}
+
+
 #[put("/<id>", data = "<scan>")]
-fn update(id: i32, scan: Json<Scan>) -> Json<Scan> {
-    scan
+fn update(id: i32, scan: Json<Scan>) -> Json<Value> {
+    let updated_scan = Scan { id: Some(id), ..scan.into_inner() };
+    Json(json!({ "status" : Scan::update(id, updated_scan, &establish_connection()) }))
 }
 
 #[delete("/<id>")]
 fn delete(id: i32) -> Json<Value> {
-    Json(json!({"status" : "ok"}))
+    let result = diesel::delete(scans::table.find(id)).execute(&establish_connection());
+
+    match result {
+        Ok(val) => {
+            Json(json!({"status" : "ok"}))
+        },
+        Err(message) => {
+            Json(json!({ "status" : "failed", "message" : format!("{}",message) }))
+        }
+    }
 }
 
-#[get("/<name>/<age>")]
-fn hello(name: String, age: u8) -> String {
-    format!("Hello, {} year old named {}!",age, name)
+#[get("/")]
+fn hello() -> String {
+    format!("Hello, friend.")
+}
+
+fn scan_exists(id: i32) -> Option<i32> {
+    let existing_scans = scans::table.filter(scans::id.eq(id)).load::<Scan>(&establish_connection());
+
+    match existing_scans {
+        Ok(found_scans) => {
+            if found_scans.len() == 1 {
+                Some(id)
+            } else {
+                None
+            }
+        },
+        Err(message) => {
+            println!("Error querying the database, which does not give me much hope for the code ahead!\n");
+            println!("{}",message);
+            None
+        }
+    }
 }
 
 fn main() {
 
-    let scan_one = Scan {
-        id: Some(1),
-        data: "{ \"scan\" : \"1\" }".to_string()
-    };
+    // Before launching the web service, seed the database with scans if they are not already present.
+    // 
+    // NOTE: This could be done in the migration but it was a useful exercise to learn how 
+    //   perform the query and work with the results.
 
-    diesel::insert_into(scans::table)
-        .values(&scan_one)
-        .execute(&establish_connection())
-        .expect("Error saving example description");
- 
+    // Search for a scan with the id of 1, 
+    // if the query is successful and the number of scans found is 0, then go ahead an insert the seed data
+    // if there was an error display a message about the dire future ahead.
+
+    match scan_exists(1) {
+        Some(id) => {
+            println!("DATABASE - Example scans already present.");
+        },
+        None => {
+            println!("DATABASE - Seeding with example scan.");
+
+            let new_scan = Scan { 
+                    id: Some(1), 
+                    data: "{ \"scan\" : \"1\" }".to_string() 
+                };
+
+            diesel::insert_into(scans::table)
+                .values(&new_scan)
+                .execute(&establish_connection())
+                .expect(&format!("Error saving scan {:?} to the database", new_scan.id));
+            }
+    }
+
+    // Now all that is left is to start the web server with the various functions
+    // we have created mounted to the appropriate paths.
 
     rocket::ignite()
-        .mount("/scans", routes![create, read, update, delete])
-        .mount("/hello",routes![hello])
+        .mount("/",routes![hello])
+        .mount("/scans", routes![index, create, read, update, delete])
         .launch();
 
-
-   
 }
--- src/scan.rs
+++ src/scan.rs
@@ -1,8 +1,26 @@
 use super::schema::scans;
+use diesel::prelude::*;
+use diesel;
+use diesel::SqliteConnection;
 
 #[table_name = "scans"]
-#[derive(Serialize, Deserialize, Queryable, Insertable)]
+#[derive(Serialize, Deserialize, Queryable, Insertable, AsChangeset)]
 pub struct Scan {
     pub id: Option<i32>,
     pub data: String
+}
+
+impl Scan {
+    pub fn create(scan: Scan, connection: &SqliteConnection) -> Scan {
+        diesel::insert_into(scans::table)
+            .values(&scan)
+            .execute(connection)
+            .expect(&format!("Error creating the scan {:?}",scan.id));
+
+        scans::table.order(scans::id.desc()).first(connection).unwrap()
+    }
+
+    pub fn update(id: i32, scan: Scan, connection: &SqliteConnection) -> bool {
+        diesel::update(scans::table.find(id)).set(&scan).execute(connection).is_ok()
+    }
 }
